939a7f033d87c0617cb2f7397af45e29a04d2ea3
Stephen Zavaleta <sjzavaleta@gmail.com>
2025-04-27 20:38:16 -0700

    Fixing up data refresher

--- src/robovisor/scripts/data_refresher.py
+++ src/robovisor/scripts/data_refresher.py
@@ -1,7 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
+
 from robovisor import create_app
 from robovisor.datacollectors.collector import refresh_db
 
-app = create_app()
+if __name__ == "__main__":
+    app = create_app()
 
-with app.app_context():
-    refresh_db()
+    with app.app_context():
+        refresh_db()
